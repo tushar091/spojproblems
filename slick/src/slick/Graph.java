61161505bf6e0d362afe9eca28aa97eb2206c9c7
Tushar <saha.tushar091@gmail.com>
2016-11-01 00:19:36 +0530

    slick

--- slick/src/slick/Graph.java
+++ slick/src/slick/Graph.java
@@ -1,5 +1,18 @@
 package slick;
 
+import java.util.LinkedList;
+
 public class Graph {
+	private int V;
+	private LinkedList<Integer> adj[];
+	
+	public Graph(int v){
+		V = v;
+		adj = new LinkedList[V];
+		for(int i = 0;i<v;i++){
+			
+		}
+	}
+	
 	
 }
