f4cca2f2b97061b5c409e02811a1b83442ef08c8
Tushar <saha.tushar091@gmail.com>
2016-11-06 21:11:54 +0530

    shortest path

--- slick/src/slick/ShPath.java
+++ slick/src/slick/ShPath.java
@@ -1,37 +1,76 @@
 package slick;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ShPath {
 	public static int V = 0;
-	
-	int minDistance(int[] dist,Boolean spSet[]){
+
+	/*
+	 * returns index of minimum distance vertex
+	 */
+	int minDistance(int[] dist, Boolean spSet[]) {
 		return 0;
 	}
-	
-	void djiskstra(int graph[][],int src){
-		int dist[] = new int[V]; 
-		
+
+	void djiskstra(int graph[][], int src) {
+		int dist[] = new int[V];
+
 		Boolean spSet[] = new Boolean[V];
-		
-		for(int i = 0;i<V;i++){
+
+		for (int i = 0; i < V; i++) {
 			dist[i] = Integer.MAX_VALUE;
 			spSet[i] = false;
 		}
 		dist[src] = 0;
-		
-		for(int count = 0;count<V-1;count++){
-			int u = minDistance(dist,spSet);
-			
-			for(int v = 0;v<V;v++){
-				if(!spSet[v] && graph[u][v] != 0 && )
+
+		for (int count = 0; count < V - 1; count++) {
+			int u = minDistance(dist, spSet);
+
+			for (int v = 0; v < V; v++) {
+				if (!spSet[v] && graph[u][v] != 0 && dist[u] + graph[u][v] < dist[v] && dist[v] != Integer.MAX_VALUE) {
+					dist[v] = dist[u] + graph[u][v];
+				}
 			}
 		}
-		
-		
+
 	}
-	
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t;
+		t = scan.nextInt();
+		ShPath sh = new ShPath();
+		while (t > 0) {
+			t--;
+			int n;
+			n = scan.nextInt();
+			int graph[][] = new int[n][n];
+			HashMap<String, Integer> nameInt = new HashMap();
+			for (int i = 0; i < n; i++) {
+				scan.nextLine();
+				String name = scan.nextLine();
+				//scan.nextLine();
+				nameInt.put(name, i);
+				int count = scan.nextInt();
+				while (count > 0) {
+					count--;
+					int a = scan.nextInt();
+					int b = scan.nextInt();
+					graph[i][a-1] = b;
+				}
+
+			}
+			String s = scan.nextLine();
+			String[] s1 = s.split(" ");
+			sh.djiskstra(graph, nameInt.get(s1[0]));
+			for(int i = 0;i<n;i++){
+				for(int j = 0;j<n;j++){
+					System.out.print(graph[i][j] + " ");
+				}
+				System.out.println("");
+			}
+		}
 	}
 
 }
